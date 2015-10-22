749e76d94e22c6f49de59461a58f16995a40863c
cochranepaul <pmcochrane@gmail.com>
2015-10-21 21:43:33 -0700

    sets up initial server.js file

---
+++ api/server.js
@@ -0,0 +1,22 @@
+var express 	= require( 'express' ),
+	app			= express (),
+	bodyParser	= require( 'body-parser'),
+	morgan		= require( 'morgan' ),
+	port 		= process.env.PORT || 8080,
+	mongoose 	= require( 'mongoose' ),
+	cors		= require( 'cors' );
+
+
+mongoose.connect( 'mongodb://localhost:27017/imagxchange')
+
+//sets up middleware
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+app.use(morgan('dev'))
+
+//app.use('/api', apiRouter) //sends any get request with api prefix to the api router
+
+app.listen(port)
+console.log("listening on port" + port)

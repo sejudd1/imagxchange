21631089f5d1726e02bc319dfb70f8722f5ed3a1
Steve Judd <sejudd1@github.com>
2015-10-22 20:20:01 -0700

    initial commit, works on view templates,links api and client foders with path

---
+++ client/js/scripts/app.js
@@ -0,0 +1,48 @@
+angular
+	.module('imagXchange', [ 'ui.router'] )
+	.config(interceptor)
+	.config(MainRouter)
+
+function interceptor($httpProvider){
+	$httpProvider.interceptors.push( 'authInterceptorFactory' )
+}
+
+function MainRouter($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise("/");
+
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'templates/home.html'
+		})
+
+		//add other pages for mainRouter here
+		
+		.state('loggedOut', {
+			url: '/loggedOut',
+			templateUrl: 'templates/home.html'
+		})
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'templates/signup.html'
+		})
+		.state('login', {
+			url: '/login',
+			templateUrl: 'templates/login.html'
+		})
+		.state('profile', {
+			url: '/profile',
+			templateUrl: 'templates/profile.html'
+		})
+		.state('gallery', {
+			url: '/gallery',
+			templateUrl: 'templates/gallery.html'
+		})
+		.state('contact', {
+			url: '/contact',
+			templateUrl: 'templates/contact.html'
+		})
+
+
+
+}

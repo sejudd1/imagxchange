5d76dbe50b156999954204ec8222b28599b77be3
Steve Judd <sejudd1@github.com>
2015-10-26 16:05:43 -0700

    adds git ignore file

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ var express 		= require( 'express' ),
 	userapiRouter	= require( './api/routes/userRoutes' ),
 	photoapiRouter	= require( './api/routes/photoRoutes' ),
 	cors			= require( 'cors' ),
-	path			= require( 'path');
+	path			= require( 'path' ),
 	//define route handler
-	http			= require( 'http').Server(app);
+	http			= require( 'http' ).Server(app),
+	//define twitter handler
+	Twit            = require( 'twit' );
+	
+
+var twitter	= new Twit({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var stream = twitter.stream('statuses/filter', { track: 'javascript' });
 
 
 
@@ -49,9 +61,30 @@ http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
 
-//listening on the port, magic is happening
-//app.listen(port)
-console.log("listening on port" + port)
+server.listen(port)
+console.log("Server started on " + port);
+var io = require('socket.io')(server);
+
+//setting up server side websocket for twitter
+io.on('connect', function(socket) {
+	stream.on('tweet', function(tweet) {
+		socket.emit('tweets', tweet);
+	});
+});
+
+//tiddy up tweet data
+stream.on('tweet', function (tweet) {
+var data = {};
+	data.name = tweet.user.name;
+	data.screen_name = tweet.user.screen_name;
+	data.text = tweet.text;
+	data.user_profile_image = tweet.user.profile_image_url;
+	socket.emit('tweets', data);
+	// setTimeout(function) {
+	// 	socket.disconnect("Die")
+	// }, 3000);
+
+});
 
 module.exports = app;
 
